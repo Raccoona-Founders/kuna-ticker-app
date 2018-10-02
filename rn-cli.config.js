239fc72ddf5a46346eade4fd6685d8de7d4b7210
Maksym Tymchyk <reilag69@gmail.com>
2018-10-02 23:09:39 +0300

    Fix building of application

--- rn-cli.config.js
+++ rn-cli.config.js
@@ -2,6 +2,9 @@ var nodejsModules = require('node-libs-react-native');
 nodejsModules.vm = require.resolve('vm-browserify');
 
 module.exports = {
+    getTransformModulePath() {
+        return require.resolve('react-native-typescript-transformer');
+    },
     extraNodeModules: nodejsModules,
     getSourceExts: () => ['jsx', 'mjs', 'js', 'ts', 'tsx'],
 };

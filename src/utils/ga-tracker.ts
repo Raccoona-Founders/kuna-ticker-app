8691c820e1b9431c3a241d419a1c441b2263f85a
Maksym Tymchyk <maksym.tymchyk@gmail.com>
2018-11-15 18:56:42 +0200

    Added Mark that define a Application Version

--- src/utils/ga-tracker.ts
+++ src/utils/ga-tracker.ts
@@ -1,5 +1,7 @@
 import firebase from 'react-native-firebase';
 
+firebase.analytics().setUserProperty('ENV', 'TEST');
+
 export function trackScreen(screenName: string | null, screenClassOverride?: string) {
     firebase.analytics().setCurrentScreen(screenName, screenClassOverride);
 }
